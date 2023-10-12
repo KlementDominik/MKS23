c2eec0a41d15e5e7eb97dc3ed4f364321d1521cb
KlementDominik <klemdom@gmail.com>
2023-10-12 15:33:07 +0200

    Support rotacniho enkoderu, meni hodnotu led displeje  podle otaceni enkoderu.

--- LEDDisplej/Core/Src/sct.c
+++ LEDDisplej/Core/Src/sct.c
@@ -10,24 +10,6 @@
 #include "main.h"
 #include "sct.h"
 
-void sct_init(void){
-	sct_led(0);
-}
-
-void sct_led(uint32_t value){
-	HAL_GPIO_WritePin(SCT_NOE_GPIO_Port, SCT_NOE_Pin, 0);				//Output enable
-	for (uint8_t i = 0; i < 32; i++)
-	{
-		HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, value & 1);	//bitova maska 0bxxxX & 0b0001
-		value >>= 1;													//posunu na dalsi bit
-		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 1);			//clk
-		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 0);
-	}
-	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 1);				//latch
-	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 0);
-}
-
-void sct_value(uint16_t value){
 	static const uint32_t reg_values[3][10] = {
 	{
 		//PCDE--------GFAB @ DIS1
@@ -70,6 +52,24 @@ void sct_value(uint16_t value){
 	},
 	};
 
+void sct_init(void){
+	sct_led(0);
+}
+
+void sct_led(uint32_t value){
+	HAL_GPIO_WritePin(SCT_NOE_GPIO_Port, SCT_NOE_Pin, 0);				//Output enable
+	for (uint8_t i = 0; i < 32; i++)
+	{
+		HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, value & 1);	//bitova maska 0bxxxX & 0b0001
+		value >>= 1;													//posunu na dalsi bit
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 1);			//clk
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 0);
+	}
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 1);				//latch
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 0);
+}
+
+void sct_value(uint16_t value){
 	uint32_t reg = 0;							//initialize to 0
 
 	reg |= reg_values[0][value / 100 % 10];		//for hundred
