618bdb41f261431634724a27799f69bd77fbb7ac
KlementDominik <klemdom@gmail.com>
2023-10-19 16:13:56 +0200

    fixed sct.c lib and sct.h

--- cv04-ADC/Core/Inc/sct.h
+++ cv04-ADC/Core/Inc/sct.h
@@ -1,15 +1,16 @@
 /*
  * sct.h
  *
- *  Created on: 15. 10. 2023
- *      Author: xkleme13
+ *  Created on: Oct 12, 2023
+ *      Author: 217410
  */
 
-#ifndef SCT_H_
-#define SCT_H_
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
 
-void sct_led(uint32_t value);
+// Function prototypes
 void sct_init(void);
-void sct_value(uint16_t value, uint8_t led);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value,uint8_t led);
 
-#endif /* SCT_H_ */
+#endif /* INC_SCT_H_ */
